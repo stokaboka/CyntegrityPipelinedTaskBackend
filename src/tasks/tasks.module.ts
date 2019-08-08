707091b70a4ca5d4b7369469a27c1483c7bd23ab
stokaboka <igothorev@gmail.com>
2019-08-08 13:53:08 +0300

    feat: add tasks controller

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
 
 @Module({
-  providers: [TasksService]
+  providers: [TasksService],
+  controllers: [TasksController]
 })
 export class TasksModule {}
