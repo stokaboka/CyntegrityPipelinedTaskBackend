86180a6e8841c56fb4c6d71d2eebecfb3a9b4083
stokaboka <igothorev@gmail.com>
2019-08-08 17:10:05 +0300

    feat: implement users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Query} from '@nestjs/common';
+import {UsersService} from "./users.service";
+import {Users} from "./users.interface";
+import {UsersDto} from "./users.dto";
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor (private readonly usersService: UsersService){}
+
+    @Get()
+    find(@Param() params): Promise<Users[]> {
+        return this.usersService.find();
+    }
+
+    @Post()
+    create(@Body() user:UsersDto): Promise<Users> {
+        return this.usersService.create(user);
+    }
+}
