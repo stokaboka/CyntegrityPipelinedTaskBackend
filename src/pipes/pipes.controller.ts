3e2b5ed5556a162876217a0f34c4fc3a93a0ea84
stokaboka <igothorev@gmail.com>
2019-08-08 17:18:50 +0300

    feat: implement pipes controller

--- src/pipes/pipes.controller.ts
+++ src/pipes/pipes.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Post, Param} from '@nestjs/common';
+import {PipesService} from "./pipes.service";
+import {Pipes} from "./pipes.interface";
+import {PipesDto} from "./pipes.dto";
 
 @Controller('pipes')
-export class PipesController {}
+export class PipesController {
+    constructor(private readonly pipesService: PipesService){}
+
+    @Get()
+    find(@Param() params): Promise<Pipes[]>{
+        return this.pipesService.find();
+    }
+
+    @Post()
+    create(@Body() pipe:PipesDto): Promise<Pipes>{
+        return this.pipesService.create(pipe);
+    }
+}
