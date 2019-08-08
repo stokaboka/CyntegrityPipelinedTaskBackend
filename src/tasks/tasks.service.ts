0c02c341988639ac8bfe715db90c88f99813fb54
stokaboka <igothorev@gmail.com>
2019-08-08 14:33:50 +0300

    feat: inject models

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,17 @@
+import { Inject, Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Users } from './interfaces/users.interface';
+import { Tasks } from './interfaces/tasks.interface';
+import {Pipes } from "./interfaces/pipes.interface";
+import {PipeTasks} from "./interfaces/pipeTasks.interface";
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectModel('Tasks') private readonly usersModel: Model<Users>,
+    @InjectModel('Users') private readonly tasksModel: Model<Tasks>,
+    @InjectModel('Pipes') private readonly pipesModel: Model<Pipes>,
+    @InjectModel('PipeTasks') private readonly pipeTasksModel: Model<PipeTasks>,
+  ) {}
+}
