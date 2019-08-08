9c32db8a664788fbf41c7cd3c627d87ba0363ca1
stokaboka <igothorev@gmail.com>
2019-08-08 18:37:48 +0300

    feat: add config module

---
+++ src/config/config.module.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019. Igor Khorev <igorhorev@gmail.com> http://orangem.me
+ */
+
+import { Module, Global } from '@nestjs/common';
+import { ConfigService } from './config.service';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: ConfigService,
+      useValue: new ConfigService(`${process.env.NODE_ENV || 'development'}.env`),
+    },
+  ],
+  exports: [ConfigService],
+})
+export class ConfigModule {}
---
+++ src/config/config.service.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2019. Igor Khorev <igorhorev@gmail.com> http://orangem.me
+ */
+
+import { Injectable } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import * as Joi from '@hapi/joi';
+import * as fs from 'fs';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import * as path from 'path';
+import {MongooseModuleOptions, MongooseOptionsFactory} from "@nestjs/mongoose";
+
+export interface EnvConfig {
+    [key: string]: string;
+}
+
+@Injectable()
+export class ConfigService implements MongooseOptionsFactory {
+    private readonly envConfig: EnvConfig;
+
+    constructor(filePath: string) {
+        const config = dotenv.parse(fs.readFileSync(filePath));
+        this.envConfig = this.validateInput(config);
+    }
+// mongodb://root:1234567890@localhost:27017/cpt
+// mongodb://localhost/cpt
+    private validateInput(envConfig: EnvConfig): EnvConfig {
+        const envVarsSchema: Joi.ObjectSchema = Joi.object({
+            NODE_ENV: Joi.string()
+                .valid(['development', 'production', 'test', 'provision'])
+                .default('development'),
+            API_VERSION: Joi.number().default(1),
+            SERVER_PORT: Joi.number().default(4444),
+            SERVER_HOST: Joi.string().default('0.0.0.0'),
+            PUBLIC_PATH: Joi.string().default(''),
+            DB_URI: Joi.string().default('mongodb://localhost/cpt'),
+        });
+
+        const { error, value: validatedEnvConfig } = Joi.validate(
+            envConfig,
+            envVarsSchema,
+        );
+        if (error) {
+            throw new Error(`Config validation error: ${error.message}`);
+        }
+        return validatedEnvConfig;
+    }
+
+    createMongooseOptions(): MongooseModuleOptions {
+        const out: any =  {
+            type: 'mongodb',
+            uri: String(this.envConfig.DB_URI),
+        };
+        // tslint:disable-next-line:no-console
+        console.log(out);
+        return out;
+    }
+
+}
