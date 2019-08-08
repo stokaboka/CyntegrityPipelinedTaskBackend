5d3e303d6fc3a0a6f9acc29eac3e45e7cc40505e
stokaboka <igothorev@gmail.com>
2019-08-08 13:51:06 +0300

    feat: add tasks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
