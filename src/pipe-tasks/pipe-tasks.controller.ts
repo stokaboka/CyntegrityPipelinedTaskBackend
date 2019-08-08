f93c50d874068bd1a0ea04a1f1caae4b31aa8099
stokaboka <igothorev@gmail.com>
2019-08-08 17:25:08 +0300

    feat: implement pipe-tasks controller

--- src/pipe-tasks/pipe-tasks.controller.ts
+++ src/pipe-tasks/pipe-tasks.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {PipeTasksService} from "./pipe-tasks.service";
+import {PipeTasks} from "./pipe-tasks.interface";
+import {PipeTasksDto} from "./pipe-tasks.dto";
 
 @Controller('pipe-tasks')
-export class PipeTasksController {}
+export class PipeTasksController {
+    constructor(private readonly pipeTasksService: PipeTasksService){}
+
+    @Get()
+    find(@Param() params): Promise<PipeTasks[]>{
+        return this.pipeTasksService.find();
+    }
+
+    @Post()
+    create(@Body() pipeTask: PipeTasksDto): Promise<PipeTasks>{
+        return this.pipeTasksService.create(pipeTask);
+    }
+}
